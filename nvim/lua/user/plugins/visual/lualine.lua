63a463a4a5ed54dad77197e224dc97edf8545e96
Michael Strobel <mstrobel97@gmail.com>
2022-05-09 10:26:08 +0200

    removed separator in lualine;

--- nvim/lua/user/plugins/visual/lualine.lua
+++ nvim/lua/user/plugins/visual/lualine.lua
@@ -2,6 +2,8 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
+    section_separators = '',
+    component_separators = '',
     disabled_filetypes = {},
     always_divide_middle = true,
     globalstatus = false,

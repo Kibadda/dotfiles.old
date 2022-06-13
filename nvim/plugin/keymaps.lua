cdd71dfee881485da028392ecbe730e21347590e
Michael Strobel <mstrobel97@gmail.com>
2022-06-13 15:03:01 +0200

    add keymap to open selected file in lazygit

--- nvim/plugin/keymaps.lua
+++ nvim/plugin/keymaps.lua
@@ -7,22 +7,29 @@ vim.keymap.set("v", "Y", "myY`y")
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 
-vim.keymap.set("n", "<A-j>", "<CMD>lua NextBuffer()<CR>")
-vim.keymap.set("n", "<A-k>", "<CMD>lua PrevBuffer()<CR>")
-vim.keymap.set("i", "<A-j>", "<ESC><CMD>lua NextBuffer()<CR>")
-vim.keymap.set("i", "<A-k>", "<ESC><CMD>lua PrevBuffer<CR>")
+vim.keymap.set({ "n", "i" }, "<A-j>", "<ESC><CMD>lua NextBuffer()<CR>")
+vim.keymap.set({ "n", "i" }, "<A-k>", "<ESC><CMD>lua PrevBuffer()<CR>")
 vim.keymap.set("n", "<A-q>", "<CMD>lua DelBuffer()<CR>")
 vim.keymap.set("n", "<Leader>Q", "<CMD>bufdo bdelete<CR>")
 
 vim.keymap.set("n", "<Leader>K", "<CMD>nohlsearch<CR>")
 
-vim.keymap.set("v", "<C-r>", '"hy<CMD>%s/<C-r>h/')
+vim.keymap.set("v", "<C-r>", '"hy:%s/<C-r>h/')
 
 vim.keymap.set("t", "*<ESC>", "<C-\\><C-n>")
 vim.keymap.set({ "t", "n" }, "<C-h>", "<C-\\><C-n><C-w>h")
 vim.keymap.set({ "t", "n" }, "<C-j>", "<C-\\><C-n><C-w>j")
 vim.keymap.set({ "t", "n" }, "<C-k>", "<C-\\><C-n><C-w>k")
 vim.keymap.set({ "t", "n" }, "<C-l>", "<C-\\><C-n><C-w>l")
+-- TODO: open selected file in lazygit in nvim
+-- TODO: should switch to "nvim --remote" or "nvim-remote"
+vim.keymap.set("t", "*o", function()
+  vim.api.nvim_chan_send(vim.b.terminal_job_id, "q")
+  local file = vim.fn.getreg "+"
+
+  vim.cmd [[sleep 100m]]
+  vim.cmd("e " .. file)
+end)
 
 vim.keymap.set("i", "<S-CR>", "<C-o>o")
 vim.keymap.set("i", "<C-CR>", "<C-o>O")

35e11aa48fbac8910734e8264de6bf0adcf49a06
Michael Strobel <mstrobel97@gmail.com>
2022-06-14 15:29:00 +0200

    friendship ended with telescope-file-browser, now ranger is my best friend

--- nvim/plugin/keymaps.lua
+++ nvim/plugin/keymaps.lua
@@ -16,21 +16,6 @@ vim.keymap.set("n", "<Leader>K", "<CMD>nohlsearch<CR>")
 
 vim.keymap.set("v", "<C-r>", '"hy:%s/<C-r>h/')
 
-vim.keymap.set("t", "*<ESC>", "<C-\\><C-n>")
-vim.keymap.set({ "t", "n" }, "<C-h>", "<C-\\><C-n><C-w>h")
-vim.keymap.set({ "t", "n" }, "<C-j>", "<C-\\><C-n><C-w>j")
-vim.keymap.set({ "t", "n" }, "<C-k>", "<C-\\><C-n><C-w>k")
-vim.keymap.set({ "t", "n" }, "<C-l>", "<C-\\><C-n><C-w>l")
--- TODO: open selected file in lazygit in nvim
--- TODO: should switch to "nvim --remote" or "nvim-remote"
-vim.keymap.set("t", "*o", function()
-  vim.api.nvim_chan_send(vim.b.terminal_job_id, "q")
-  local file = vim.fn.getreg "+"
-
-  vim.cmd [[sleep 100m]]
-  vim.cmd("e " .. file)
-end)
-
 vim.keymap.set("i", "<S-CR>", "<C-o>o")
 vim.keymap.set("i", "<C-CR>", "<C-o>O")
 
@@ -40,3 +25,9 @@ vim.keymap.set("n", "<A-t>", "<C-w>+")
 vim.keymap.set("n", "<A-s>", "<C-w>-")
 vim.keymap.set("n", "<C-,>", "<C-w><")
 vim.keymap.set("n", "<C-.>", "<C-w>>")
+
+vim.keymap.set("t", "*<ESC>", "<C-\\><C-n>")
+vim.keymap.set({ "t", "n" }, "<C-h>", "<C-\\><C-n><C-w>h")
+vim.keymap.set({ "t", "n" }, "<C-j>", "<C-\\><C-n><C-w>j")
+vim.keymap.set({ "t", "n" }, "<C-k>", "<C-\\><C-n><C-w>k")
+vim.keymap.set({ "t", "n" }, "<C-l>", "<C-\\><C-n><C-w>l")

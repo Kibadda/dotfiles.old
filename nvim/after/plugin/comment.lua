3df16fd67b1e6ee91ab32e239e1085fbe30e3e6b
Michael Strobel <mstrobel97@gmail.com>
2022-06-24 15:51:54 +0200

    ignore empty lines for commenting

--- nvim/after/plugin/comment.lua
+++ nvim/after/plugin/comment.lua
@@ -18,9 +18,8 @@ require("Comment").setup {
   },
   pre_hook = nil,
   post_hook = nil,
-  ignore = nil,
+  ignore = "^$",
 }
 
 local comment_ft = require "Comment.ft"
-comment_ft.set("lua", { "--%s", "--[[%s]]" })
-comment_ft.set("smarty", { "{* %s *}", "{* %s *}" })
+comment_ft.set("smarty", { "{*%s*}", "{*%s*}" })

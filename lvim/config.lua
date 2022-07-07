d5944d11917b2f930a19f0b7224685200560f9c5
Michael Strobel <mstrobel97@gmail.com>
2022-07-07 17:18:33 +0200

    lvim settings must be in config.lua

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,3 +1,23 @@
+-- LunarVim settings
+lvim.line_wrap_cursor_movement = false
+-- :PackerInstall :PackerCompile
+lvim.builtin.alpha.active = false
+lvim.builtin.notify.active = true
+
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "yaml",
+  "php",
+  "vue",
+}
+
 local _, actions = pcall(require, "telescope.actions")
 lvim.builtin.telescope.defaults.mappings = {
   i = {
--- lvim/plugin/settings.lua
+++ lvim/plugin/settings.lua
@@ -2,23 +2,3 @@
 vim.opt.relativenumber = true
 vim.opt.mouse = "nv"
 vim.opt.whichwrap = "b,s"
-
--- LunarVim settings
-lvim.line_wrap_cursor_movement = false
--- :PackerInstall :PackerCompile
-lvim.builtin.alpha.active = false
-lvim.builtin.notify.active = true
-
-lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "yaml",
-  "php",
-  "vue",
-}

ba95d0cceced60a148fba236b6a4698be8a7d6ae
Michael Strobel <mstrobel97@gmail.com>
2022-07-18 07:51:59 +0200

    moved terminal opt_local stuff

--- lvim/plugin/autocmds.lua
+++ lvim/plugin/autocmds.lua
@@ -3,26 +3,22 @@ local TerminalGroup = vim.api.nvim_create_augroup("Terminal", { clear = true })
 vim.api.nvim_create_autocmd("TermOpen", {
   group = TerminalGroup,
   pattern = "*",
-  callback = function()
-    vim.opt_local.filetype = "term"
-    vim.opt_local.number = false
-    vim.opt_local.relativenumber = false
-    vim.opt_local.signcolumn = "no"
-    vim.opt_local.spell = false
-    vim.cmd [[startinsert]]
-  end,
+  command = [[
+    set ft=terminal
+    startinsert
+  ]],
 })
 
 vim.api.nvim_create_autocmd("BufEnter", {
   group = TerminalGroup,
   pattern = "term://*",
-  command = "startinsert",
+  command = [[startinsert]],
 })
 
 vim.api.nvim_create_autocmd("TermClose", {
   group = TerminalGroup,
   pattern = "*",
-  command = "bdelete!",
+  command = [[bdelete!]],
 })
 
 local StartifyGroup = vim.api.nvim_create_augroup("Startify", { clear = true })
@@ -30,7 +26,5 @@ local StartifyGroup = vim.api.nvim_create_augroup("Startify", { clear = true })
 vim.api.nvim_create_autocmd("User", {
   group = StartifyGroup,
   pattern = "StartifyReady",
-  callback = function()
-    vim.cmd [[cd ~]]
-  end,
+  command = [[cd ~]],
 })

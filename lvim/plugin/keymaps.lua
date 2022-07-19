a7f1acdba5ab99cd19a5e02d61b3ddc92989afc1
Michael Strobel <mstrobel97@gmail.com>
2022-07-19 10:02:26 +0200

    better keymaps for buffer switching

--- lvim/plugin/keymaps.lua
+++ lvim/plugin/keymaps.lua
@@ -1,7 +1,7 @@
 vim.keymap.set("n", "<A-w>", "<cmd>silent w<cr>")
 vim.keymap.set("n", "<A-q>", "<cmd>BufferKill<cr>")
-vim.keymap.set({ "n", "i", "t" }, "<A-l>", "<cmd>BufferLineCycleNext<cr>")
-vim.keymap.set({ "n", "i", "t" }, "<A-h>", "<cmd>BufferLineCyclePrev<cr>")
+vim.keymap.set("t", "L", "<cmd>BufferLineCycleNext<cr>")
+vim.keymap.set("t", "H", "<cmd>BufferLineCyclePrev<cr>")
 vim.keymap.set("n", "<A-f>", "<cmd>ToggleFormatOnSave<cr>")
 
 lvim.builtin.which_key.mappings["S"] = {

cd1e13d788406281f7a433c0d6a9765039f4be01
Michael Strobel <mstrobel97@gmail.com>
2022-08-10 11:47:03 +0200

    better looking sections with padding and responsive icons

--- xmobar/xmobar.hs
+++ xmobar/xmobar.hs
@@ -3,8 +3,29 @@ import Xmobar
 
 type Extension = (Runnable, String)
 
+type Color = String
+type Text = String
+type Action = String
+type Icon = String
+
+myTerminal :: String
+myTerminal = "kitty"
+
+wrap :: String -> String -> String -> String
+wrap _ _ "" = ""
+wrap l r m = l <> m <> r
+
+xmobarColor :: Color -> Color -> Text -> String
+xmobarColor fg bg = wrap open "</fc>"
+  where
+    open :: String
+    open = concat ["<fc=", fg, if null bg then "" else "," <> bg, ">"]
+
+xmobarIcon :: Icon -> String
+xmobarIcon = wrap "<fn=2>" "</fn>"
+
 separator :: String
-separator = "  "
+separator = wrap "<fn=1> " " </fn>" "|"
 
 alignSeparator :: String
 alignSeparator = "}{"
@@ -18,37 +39,66 @@ leftSide =
 
 rightSide :: [Extension]
 rightSide =
-  [ ( Run Locks,
-      "<box type=Bottom width=2 mb=2 color=#3434e0><fc=#3434e0><fn=2>\xf023</fn> %locks%</fc></box>"
+  [ ( Run $ DynNetwork
+        [ "-t", "<dev>: <tx>|<rx>"
+	] 10,
+      xmobarColor "#E03434" "" $ xmobarIcon "\xf1eb" ++ " %dynnetwork%"
     ),
-    ( Run $ Cpu ["-t", "<fn=2>\xf108</fn> cpu: <total>%", "-H", "50", "--high", "red"] 20,
-      "<box type=Bottom width=2 mb=2 color=#b8bb26><fc=#b8bb26><action=`kitty htop`>%cpu%</action></fc></box>"
+    ( Run Locks,
+      xmobarColor "#3434E0" "" $ xmobarIcon "\xf023" ++ " %locks%"
     ),
-    ( Run $ Memory ["-t", "<fn=2>\xf233</fn> mem: <used>M <usedratio>%"] 20,
-      "<box type=Bottom width=2 mb=2 color=#458588><fc=#458588><action=`kitty htop`>%memory%</action></fc></box>"
+    ( Run $ Com ".local/bin/pacupdate" [] "pacupdate" 3000,
+      xmobarColor "#689D6A" "" $ xmobarIcon "\xf0f3" ++ " %pacupdate%"
     ),
-    ( Run $ DynNetwork ["-t", "<fn=2>\xf1eb</fn> <dev>: <tx>|<rx>"] 10,
-      "<box type=Bottom width=2 mb=2 color=#e03434><fc=#e03434>%dynnetwork%</fc></box>"
+    ( Run $ Cpu
+        [ "-t", "<total>%",
+	  "-H", "50",
+	  "--high", "red",
+	  "-p", "3"
+	] 20,
+      xmobarColor "#B8BB26" "" $ xmobarIcon "\xf108" ++ " %cpu%"
     ),
-    ( Run $ Com ".local/bin/pacupdate" [] "pacupdate" 36000,
-      "<box type=Bottom width=2 mb=2 color=#689d6a><fc=#689d6a><action=`kitty sudo pacman -Syu`>%pacupdate%</action></fc></box>"
+    ( Run $ Memory
+        [ "-t", "<used>M <usedratio>%",
+	  "-p", "3"
+	] 20,
+      xmobarColor "#458588" "" $ xmobarIcon "\xf233" ++ " %memory%"
     ),
-    ( Run $ BatteryP ["BAT0"] ["-t", "<fn=2>\xf242</fn> <acstatus> <left>%"] 600,
-      "<box type=Bottom width=2 mb=2 color=#fb4934><fc=#fb4934>%battery%</fc></box>"
+    ( Run $ BatteryP ["BAT0"]
+        [ "-t", "<acstatus>",
+	  "-p", "3",
+	  "--",
+	  "-a", "notify-send -u critical 'Battery Low!!'",
+	  "-o", xmobarIcon "\xf243" ++ " <left>% <timeleft>",
+	  "-O", xmobarIcon "\xf242" ++ " <left>%",
+	  "-i", xmobarIcon "\xf240" ++ " 100%"
+	] 600,
+      xmobarColor "#FB4934" "" "%battery%"
     ),
-    ( Run $ Alsa "default" "Master" ["-t", "<fn=2>\xf028</fn> <volumestatus>%"],
-      "<box type=Bottom width=2 mb=2 color=#1cfc03><fc=#1cfc03><action=`pavucontrol`>%alsa:default:Master%</action></fc></box>"
+    ( Run $ Alsa "default" "Master"
+        [ "-t", "<status>",
+	  "-p", "3",
+	  "--",
+	  "-O", " <volume>%",
+	  "-o", xmobarIcon "\xf6a9" ++ " <volume>%",
+	  "-H", "50",
+	  "-h", xmobarIcon "\xf028",
+	  "-m", xmobarIcon "\xf027",
+	  "-L", "1",
+	  "-l", xmobarIcon "\xf026"
+	],
+      xmobarColor "#1CFC03" "" "%alsa:default:Master%"
     ),
-    ( Run $ Date "<fn=2>\xf017</fn> %d.%m.%Y - %H:%M:%S" "date" 10,
-      "<box type=Bottom width=2 mb=2 color=#83a598><fc=#83a598><action=`xdg-open https://calendar.google.com`>%date%</action></fc></box>"
-    -- ),
-    -- ( Run $ Com ".local/bin/trayer-padding" [] "trayerpad" 20,
-    --   "%trayerpad%"
+    ( Run $ Date "%d.%m.%Y" "date" 36000,
+      xmobarColor "#83A598" "" $ xmobarIcon "\xf073" ++ " %date%"
+    ),
+    ( Run $ Date "%H:%M:%S" "time" 10,
+      xmobarColor "#83A598" "" $ xmobarIcon "\xf017" ++ " %time%"
     )
   ]
 
 myTemplate :: String
-myTemplate = intercalate separator (map snd leftSide) ++ alignSeparator ++ intercalate separator (map snd rightSide) ++ separator
+myTemplate = intercalate separator (map snd leftSide) ++ alignSeparator ++ intercalate separator (map snd rightSide) ++ " "
 
 myCommands :: [Runnable]
 myCommands = map fst leftSide ++ map fst rightSide

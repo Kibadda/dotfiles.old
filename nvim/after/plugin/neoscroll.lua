d74e3267e0b7a777eb709eac2d4edc3f5c8332c9
Michael Strobel <mstrobel97@gmail.com>
2022-08-25 11:25:09 +0200

    neoscroll update; rename which-key keybindings

--- nvim/after/plugin/neoscroll.lua
+++ nvim/after/plugin/neoscroll.lua
@@ -3,5 +3,24 @@ if not pcall(require, "neoscroll") then
 end
 
 require("neoscroll").setup {
-  mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+  mappings = {
+    "<C-u>",
+    "<C-d>",
+    -- "<C-b>",
+    -- "<C-f>",
+    -- "<C-y>",
+    -- "<C-e>",
+    "zt",
+    "zz",
+    "zb",
+  },
 }
+
+RegisterKeymaps("n", "", {
+  ["<C-u>"] = { "Scroll cursor up" },
+  ["<C-d>"] = { "Scroll cursor down" },
+  -- ["<C-b>"] = { "Scroll cursor up" },
+  -- ["<C-f>"] = { "Scroll cursor down" },
+  -- ["<C-y>"] = { "Scroll window up" },
+  -- ["<C-e>"] = { "Scroll window down" },
+})

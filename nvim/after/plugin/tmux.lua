13702d00b7d819ba2d599df286f8be14434607c9
Michael Strobel <mstrobel97@gmail.com>
2022-08-25 16:02:56 +0200

    add nvim keybindings to create tmux panes

--- nvim/after/plugin/tmux.lua
+++ nvim/after/plugin/tmux.lua
@@ -14,3 +14,32 @@ RegisterKeymaps("n", "", {
   ["<C-k>"] = { ntn.NvimTmuxNavigateUp, "Tmux move up" },
   ["<C-l>"] = { ntn.NvimTmuxNavigateRight, "Tmux move right" },
 })
+
+local function tmux_command(command)
+  local tmux_socket = vim.fn.split(vim.env.TMUX, ",")[1]
+  return vim.fn.system("tmux -S " .. tmux_socket .. " " .. command)
+end
+
+RegisterKeymaps("n", "<Leader>", {
+  t = {
+    name = "Tmux",
+    h = {
+      function()
+        tmux_command "split-window -h"
+      end,
+      "Split Horizontal",
+    },
+    v = {
+      function()
+        tmux_command "split-window -v"
+      end,
+      "Split Vertical",
+    },
+    c = {
+      function()
+        tmux_command "new-window"
+      end,
+      "New Window",
+    },
+  },
+})

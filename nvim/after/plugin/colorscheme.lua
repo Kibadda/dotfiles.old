61bea490514532b3b326328abd64e12fa19a97a4
Michael Strobel <mstrobel97@gmail.com>
2022-09-01 10:45:36 +0200

    remove colorscheme file

--- nvim/after/plugin/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-if not PluginsOk "colorbuddy" then
-  return
-end
-
-require("colorbuddy").colorscheme "gruvbuddy"
-
-vim.api.nvim_set_hl(0, "BufferLineBackground", { fg = "#80a0ff", bg = "#1e2127" })
-vim.api.nvim_set_hl(0, "BufferLineFill", { fg = "#80a0ff", bg = "#1e2127" })
-vim.api.nvim_set_hl(0, "BufferLineSeparator", { bg = "#1e2127" })

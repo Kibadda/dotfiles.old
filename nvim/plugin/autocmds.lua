3a65bde6f800a3bf5bb24f7091ff8eb914e7f8cd
Michael Strobel <mstrobel97@gmail.com>
2022-09-02 15:03:22 +0200

    keybinding to open plugin under cursor in plugins.lua file

--- nvim/plugin/autocmds.lua
+++ nvim/plugin/autocmds.lua
@@ -44,3 +44,18 @@ vim.api.nvim_create_autocmd("TermClose", {
   pattern = "*",
   command = "bd!",
 })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("PluginsGitOpen", { clear = true }),
+  pattern = "plugins.lua",
+  callback = function()
+    RegisterKeymaps {
+      mode = "n",
+      prefix = "",
+      buffer = 0,
+      {
+        gX = { OpenPlugin, "Open Plugin" },
+      },
+    }
+  end,
+})
--- nvim/plugin/globals.lua
+++ nvim/plugin/globals.lua
@@ -43,3 +43,58 @@ function JumpDirection(direction)
 
   vim.cmd.normal { count .. direction, bang = true }
 end
+
+---open plugin under cursor in browser
+function OpenPlugin()
+  local ts_utils = require "nvim-treesitter.ts_utils"
+  local current_node = ts_utils.get_node_at_cursor(0)
+
+  if current_node == nil then
+    return
+  end
+
+  while current_node:type() ~= "function_call" do
+    current_node = current_node:parent()
+  end
+
+  local node_string = vim.treesitter.get_node_text(current_node, 0)
+
+  if string.sub(node_string, 1, 3) ~= "use" then
+    return
+  end
+
+  local query_string = [[
+    (
+     (function_call
+       name: (identifier) @function_name
+       arguments: [
+         (arguments (string) @argument)
+         (arguments
+           (table_constructor
+             (field
+               !name
+               value: (string) @argument)))
+       ])
+    )
+  ]]
+
+  local query = vim.treesitter.parse_query("lua", query_string)
+  local root = vim.treesitter.get_string_parser(node_string, "lua", {}):parse()[1]:root()
+
+  for _, match in query:iter_matches(root, 0, 0, -1) do
+    local function_name, argument
+    for id, node in pairs(match) do
+      if query.captures[id] == "function_name" then
+        function_name = vim.treesitter.get_node_text(node, node_string)
+      end
+      if query.captures[id] == "argument" then
+        argument = vim.treesitter.get_node_text(node, node_string)
+      end
+      if function_name and argument then
+        local url = "https://github.com/" .. argument
+        os.execute("xdg-open " .. url)
+        return
+      end
+    end
+  end
+end

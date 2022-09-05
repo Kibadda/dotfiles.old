14b13b51bcfd358a63657c680f76d3be7babb0bf
Michael Strobel <mstrobel97@gmail.com>
2022-09-05 15:53:19 +0200

    i do not understand why the global setting does absolutely nothing

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -1,5 +1,3 @@
 SetOptionsLocal {
-  tabstop = 4,
-  shiftwidth = 4,
   formatoptions = vim.opt_local.formatoptions - "o",
 }
--- nvim/after/ftplugin/php.lua
+++ nvim/after/ftplugin/php.lua
@@ -1,4 +1,5 @@
 SetOptionsLocal {
   tabstop = 4,
   shiftwidth = 4,
+  formatoptions = vim.opt_local.formatoptions - "o",
 }

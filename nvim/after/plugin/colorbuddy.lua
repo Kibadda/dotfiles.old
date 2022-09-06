371f41b463138c3e34d6bcfd6778eba3f515ffe1
Michael Strobel <mstrobel97@gmail.com>
2022-09-06 08:39:43 +0200

    add highlight fix for bufferline

--- nvim/after/plugin/colorbuddy.lua
+++ nvim/after/plugin/colorbuddy.lua
@@ -7,3 +7,4 @@ require("colorbuddy").colorscheme "gruvbuddy"
 vim.api.nvim_set_hl(0, "BufferLineBackground", { fg = "#80a0ff", bg = "#1e2127" })
 vim.api.nvim_set_hl(0, "BufferLineFill", { fg = "#80a0ff", bg = "#1e2127" })
 vim.api.nvim_set_hl(0, "BufferLineSeparator", { bg = "#1e2127" })
+vim.api.nvim_set_hl(0, "BufferLineModified", { fg = "#ffa0a0", bg = "#1e2127" })

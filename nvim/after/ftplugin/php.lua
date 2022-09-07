53f413f0c776a33ed6c82576fbff7d86fbe18da5
Michael Strobel <mstrobel97@gmail.com>
2022-09-07 15:49:14 +0200

    why the f*ck does this not work for php files

--- nvim/after/ftplugin/php.lua
+++ nvim/after/ftplugin/php.lua
@@ -1,5 +1,7 @@
-SetOptionsLocal {
-  tabstop = 4,
-  shiftwidth = 4,
-  formatoptions = GetFormatOptions(vim.opt_local),
-}
+vim.defer_fn(function()
+  SetOptionsLocal {
+    tabstop = 4,
+    shiftwidth = 4,
+    formatoptions = GetFormatOptions(vim.opt_local),
+  }
+end, 100)

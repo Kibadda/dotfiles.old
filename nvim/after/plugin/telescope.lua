7e3d8ba42a2018e1b8b76a8c2edb1aeb728accc8
Michael Strobel <mstrobel97@gmail.com>
2022-09-09 15:19:55 +0200

    💪 add seperate keybind to show all emojis

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -50,35 +50,45 @@ RegisterKeymaps {
   },
 }
 
+local function terminal_emojis(source)
+  require("telescope.builtin").symbols {
+    attach_mappings = function(prompt_bufnr, map)
+      local function close_telescope(callback)
+        local actions = require "telescope.actions"
+        actions.close(prompt_bufnr)
+        vim.defer_fn(function()
+          _ = callback and callback()
+          vim.cmd.startinsert()
+        end, 0)
+      end
+      map("i", "<CR>", function()
+        close_telescope(function()
+          local state = require "telescope.actions.state"
+          vim.api.nvim_put({ state.get_selected_entry().value[1] }, "", true, true)
+        end)
+      end)
+      map("n", "<ESC>", function()
+        close_telescope()
+      end)
+      return true
+    end,
+    sources = { source },
+  }
+end
+
 RegisterKeymaps {
   mode = "t",
   prefix = "",
   {
     ["<M-e>"] = {
       function()
-        require("telescope.builtin").symbols {
-          attach_mappings = function(prompt_bufnr, map)
-            local function close_telescope(callback)
-              local actions = require "telescope.actions"
-              actions.close(prompt_bufnr)
-              vim.defer_fn(function()
-                _ = callback and callback()
-                vim.cmd.startinsert()
-              end, 0)
-            end
-            map("i", "<CR>", function()
-              close_telescope(function()
-                local state = require "telescope.actions.state"
-                vim.api.nvim_put({ state.get_selected_entry().value[1] }, "", true, true)
-              end)
-            end)
-            map("n", "<ESC>", function()
-              close_telescope()
-            end)
-            return true
-          end,
-          sources = { "gitmoji" },
-        }
+        terminal_emojis "gitmoji"
+      end,
+      "Emojis",
+    },
+    ["<M-w>"] = {
+      function()
+        terminal_emojis "emoji"
       end,
       "Emojis",
     },

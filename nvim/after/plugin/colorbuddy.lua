fc5578ade55f0040e2357b5b10bc36c4edfb0488
Michael Strobel <mstrobel97@gmail.com>
2022-09-19 09:03:55 +0200

    🧭 fix duplicate bufferline highlight

--- nvim/after/plugin/colorbuddy.lua
+++ nvim/after/plugin/colorbuddy.lua
@@ -5,6 +5,7 @@ end
 require("colorbuddy").colorscheme "gruvbuddy"
 
 vim.api.nvim_set_hl(0, "BufferLineBackground", { fg = "#80a0ff", bg = "#1e2127" })
+vim.api.nvim_set_hl(0, "BufferLineDuplicate", { fg = "#e0e0e0", bg = "#1e2127" })
 vim.api.nvim_set_hl(0, "BufferLineFill", { fg = "#80a0ff", bg = "#1e2127" })
 vim.api.nvim_set_hl(0, "BufferLineSeparator", { bg = "#1e2127" })
 vim.api.nvim_set_hl(0, "BufferLineModified", { fg = "#ffa0a0", bg = "#1e2127" })

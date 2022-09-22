5f783a89715496927c8652429af92165248cd64a
Michael Strobel <mstrobel97@gmail.com>
2022-09-22 13:05:44 +0200

    👔 always show relative path to cwd in winbar

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -33,7 +33,7 @@ SetOptions {
   signcolumn = "yes:2",
   timeoutlen = 100,
   termguicolors = true,
-  winbar = "%=%m %f:%L ",
+  winbar = "%=%m %{fnamemodify(expand('%'), ':.')}:%L ",
   formatoptions = GetFormatOptions(vim.opt),
   textwidth = 120,
 }

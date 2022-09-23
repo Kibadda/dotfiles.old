1d5f1544978a7de91ffce1e77d702b56b40abdd7
Michael Strobel <mstrobel97@gmail.com>
2022-09-23 08:26:40 +0200

    🍕 only show highest severity per lin in signs column

--- nvim/after/plugin/diagnostic.lua
+++ nvim/after/plugin/diagnostic.lua
@@ -1,24 +1,31 @@
 vim.diagnostic.config {
-  underline = true,
-  virtual_text = {
-    severity = nil,
-    source = "if_many",
-    format = nil,
-  },
-  signs = true,
-
-  float = {
-    show_header = true,
-    format = function(d)
-      local t = vim.deepcopy(d)
-      local code = d.code or (d.user_data and d.user_data.lsp and d.user_data.lsp.code) or nil
-      if code then
-        t.message = string.format("%s [%s]", t.message, code):gsub("1. ", "")
-      end
-      return t.message
-    end,
-  },
-
   severity_sort = true,
-  update_in_insert = false,
 }
+
+local ns = vim.api.nvim_create_namespace "my_namespace"
+local orig_signs_handler = vim.diagnostic.handlers.signs
+
+vim.diagnostic.handlers.signs = {
+  show = function(_, bufnr, _, opts)
+    local diagnostics = vim.diagnostic.get(bufnr)
+
+    local max_severity_per_line = {}
+    for _, d in pairs(diagnostics) do
+      local m = max_severity_per_line[d.lnum]
+      if not m or d.severity < m.severity then
+        max_severity_per_line[d.lnum] = d
+      end
+    end
+
+    local filterd_diagnostics = vim.tbl_values(max_severity_per_line)
+    orig_signs_handler.show(ns, bufnr, filterd_diagnostics, opts)
+  end,
+  hide = function(_, bufnr)
+    orig_signs_handler.hide(ns, bufnr)
+  end,
+}
+
+-- vim.cmd.sign "define DiagnosticSignError text=🔥 texthl=DiagnosticSignError linehl= numhl="
+-- vim.cmd.sign "define DiagnosticSignWarn text=🚨 texthl=DiagnosticSignWarn linehl= numhl="
+-- vim.cmd.sign "define DiagnosticSignInfo text=ℹ️ texthl=DiagnosticSignInfo linehl= numhl="
+-- vim.cmd.sign "define DiagnosticSignHint text=🚧 texthl=DiagnosticSignHint linehl= numhl="

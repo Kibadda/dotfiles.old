a4ac7c8afe03a6a1974b99f0d5122450db7b057b
Michael Strobel <mstrobel97@gmail.com>
2022-09-27 10:13:25 +0200

    🌁 (／‵Д′)／~ ╧╧

--- nvim/after/plugin/gitsigns.lua
+++ nvim/after/plugin/gitsigns.lua
@@ -2,10 +2,6 @@ if not PluginsOk "gitsigns" then
   return
 end
 
--- if not string.find(vim.fn.getcwd(), "^/media/beta/") then
--- FIX: check for yubikey lock status
--- if unlocked -> launch gpg-agent -> hook into and require following part
--- otherwise just require following part
 require("gitsigns").setup {}
 
 RegisterKeymaps {
@@ -19,7 +15,15 @@ RegisterKeymaps {
       k = { "<Cmd>Gitsigns prev_hunk<CR>", "Prev Hunk" },
       l = { "<Cmd>Gitsigns blame_line<CR>", "Blame Line" },
       s = { "<Cmd>Telescope git_status<CR>", "Status" },
-      g = { "<Cmd>Lazygit<CR>", "Lazygit" },
+      g = {
+        -- FIX: I really do not know why
+        function()
+          if not string.find(vim.fn.getcwd(), "^/media/beta/") then
+            vim.cmd.Lazygit()
+          end
+        end,
+        "Lazygit",
+      },
       b = { "<Cmd>Telescope git_branches<CR>", "Checkout Branches" },
       c = { "<Cmd>Telescope git_commits<CR>", "Checkout Commit" },
       C = { "<Cmd>Telescope git_bcommits<CR>", "Checkout Commit (current file)" },
@@ -28,4 +32,3 @@ RegisterKeymaps {
     },
   },
 }
--- end

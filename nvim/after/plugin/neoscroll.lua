3508b0703e3c136ff6a0d6bf862e054ce3245eb2
Michael Strobel <mstrobel97@gmail.com>
2022-09-29 08:15:17 +0200

    🚭 add other scroll keys

--- nvim/after/plugin/neoscroll.lua
+++ nvim/after/plugin/neoscroll.lua
@@ -6,23 +6,31 @@ require("neoscroll").setup {
   mappings = {
     "<C-u>",
     "<C-d>",
+    "<C-b>",
+    "<C-f>",
+    "<C-y>",
+    "<C-e>",
     "zt",
     "zz",
     "zb",
   },
-  pre_hook = function()
-    vim.wo.cursorline = false
-  end,
-  post_hook = function()
-    vim.wo.cursorline = true
-  end,
+  -- pre_hook = function()
+  --   vim.wo.cursorline = false
+  -- end,
+  -- post_hook = function()
+  --   vim.wo.cursorline = true
+  -- end,
 }
 
 RegisterKeymaps {
   mode = "n",
   prefix = "",
   {
-    ["<C-u>"] = { "Scroll cursor up" },
-    ["<C-d>"] = { "Scroll cursor down" },
+    ["<C-u>"] = { "Scroll cursor up half page" },
+    ["<C-d>"] = { "Scroll cursor down half page" },
+    ["<C-b>"] = { "Scroll cursor up full page" },
+    ["<C-f>"] = { "Scroll cursor down full page" },
+    ["<C-y>"] = { "Scroll page down" },
+    ["<C-e>"] = { "Scroll page up" },
   },
 }

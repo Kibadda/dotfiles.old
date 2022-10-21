e02551d041d01caada7b2a4a8309a21b4c3b6364
Michael Strobel <mstrobel97@gmail.com>
2022-10-21 10:11:25 +0200

    🤩 add IncRename mode to noice

--- nvim/after/plugin/noice.lua
+++ nvim/after/plugin/noice.lua
@@ -6,6 +6,28 @@ require("noice").setup {
   popupmenu = {
     backend = "cmp",
   },
+  cmdline = {
+    format = {
+      IncRename = {
+        pattern = "^:%s*IncRename%s+",
+        icon = " ",
+        conceal = true,
+        opts = {
+          relative = "cursor",
+          size = {
+            min_width = 20,
+          },
+          position = {
+            row = -3,
+            col = 0,
+          },
+          buf_options = {
+            filetype = "text",
+          },
+        },
+      },
+    },
+  },
   routes = {
     {
       view = "split",

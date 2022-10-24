687a99f6cb088d04fb3d147f520b18b74a7393ef
Michael Strobel <mstrobel97@gmail.com>
2022-10-24 13:33:50 +0200

    🐈 new scratch

---
+++ nvim/scratch/fs.lua
@@ -0,0 +1,11 @@
+-- for k, v in vim.fs.dir "~/.cache/laravel-docs" do
+--   vim.pretty_print { k, v }
+-- end
+
+vim.pretty_print(vim.fs.find(function(file)
+  return string.match("artisan.md", ".md$") ~= nil
+end, {
+  path = "~/.cache/laravel-docs",
+  type = "file",
+  limit = math.huge,
+}))

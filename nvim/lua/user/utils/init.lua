b5a8ca0a930fbfc44b70700143973700d402ac6a
Michael Strobel <mstrobel97@gmail.com>
2023-01-10 10:55:54 +0100

    🚓 move terminal.lua into utils/init.lua

--- nvim/lua/user/utils/init.lua
+++ nvim/lua/user/utils/init.lua
@@ -77,4 +77,38 @@ function M.set_cwd_options(value_if_work, default)
   }
 end
 
+function M.open_terminal(opts)
+  opts = vim.tbl_extend("keep", opts or {}, {
+    position = "right",
+    size = 60,
+  })
+
+  local cmd = ""
+  if opts.position == "top" or opts.position == "left" then
+    cmd = cmd .. "topleft "
+  end
+  if opts.size ~= nil then
+    cmd = cmd .. opts.size
+  end
+  if opts.position == "left" or opts.position == "right" then
+    cmd = cmd .. "v"
+  end
+  cmd = cmd .. "split"
+
+  vim.cmd(cmd)
+  local winid = vim.api.nvim_get_current_win()
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_set_current_win(winid)
+  vim.api.nvim_win_set_buf(winid, buf)
+  vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-w><C-h>", { buffer = buf })
+  vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w><C-j>", { buffer = buf })
+  vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w><C-k>", { buffer = buf })
+  vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w><C-l>", { buffer = buf })
+  vim.fn.termopen(vim.env.SHELL)
+end
+
+function M.open_url()
+  os.execute("xdg-open " .. vim.fn.expand "<cWORD>")
+end
+
 return M

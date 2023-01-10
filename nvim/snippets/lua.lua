9982848ee2003356ed1a6c033071a4e2943fe967
Michael Strobel <mstrobel97@gmail.com>
2023-01-10 10:56:24 +0100

    🧹 cleanup

--- nvim/snippets/lua.lua
+++ nvim/snippets/lua.lua
@@ -2,38 +2,8 @@ local luasnip = require "luasnip"
 local fmt = require("luasnip.extras.fmt").fmt
 local s = luasnip.snippet
 local i = luasnip.insert_node
-local n = luasnip.snippet_node
-local d = luasnip.dynamic_node
-local f = luasnip.function_node
 
 return {
-  s(
-    "pok",
-    fmt(
-      [[
-        if not plugins_ok "[]" then
-          return
-        end
-
-        require("[]").setup {[]}
-      ]],
-      {
-        d(1, function()
-          local current_file_name = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":t:r")
-          return n(nil, {
-            i(1, current_file_name),
-          })
-        end),
-        f(function(args)
-          return args[1]
-        end, { 1 }),
-        i(0),
-      },
-      {
-        delimiters = "[]",
-      }
-    )
-  ),
   s(
     "laf",
     fmt(

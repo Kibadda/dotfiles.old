8def24de2adc7f9668e328f0e9e9e39f4d9c8453
Michael Strobel <mstrobel97@gmail.com>
2023-01-11 09:56:05 +0100

    🛺 add autocmd to create missing folders

--- nvim/lua/user/autocmds.lua
+++ nvim/lua/user/autocmds.lua
@@ -44,3 +44,14 @@ vim.api.nvim_create_autocmd("TermOpen", {
     }
   end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup("AutoCreateDir", { clear = true }),
+  callback = function(event)
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+    local backup = vim.fn.fnamemodify(file, ":p:~:h")
+    backup = backup:gsub("[/\\]", "%%")
+    vim.go.backupext = backup
+  end,
+})

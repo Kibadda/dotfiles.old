945dbe6887c9609df4dd88660e03261e79b43d0f
Michael Strobel <mstrobel97@gmail.com>
2023-01-13 16:01:54 +0100

    🧭 cmp settings changes

--- nvim/lua/user/plugins/cmp.lua
+++ nvim/lua/user/plugins/cmp.lua
@@ -6,6 +6,7 @@ local M = {
     "hrsh7th/cmp-nvim-lsp",
     "L3MON4D3/LuaSnip",
     "saadparwaiz1/cmp_luasnip",
+    "onsails/lspkind.nvim",
   },
   event = "InsertEnter",
 }
@@ -13,172 +14,29 @@ local M = {
 function M.config()
   local cmp = require "cmp"
   local luasnip = require "luasnip"
+  local lspkind = require "lspkind"
 
-  local function jumpable(backwards)
-    if backwards == nil then
-      backwards = false
-    else
-      backwards = true
-    end
-    local win_get_cursor = vim.api.nvim_win_get_cursor
-    local get_current_buf = vim.api.nvim_get_current_buf
-
-    local function inside_snippet()
-      local node = luasnip.session.current_nodes[get_current_buf()]
-      if not node then
-        return false
-      end
-
-      local snip_begin_pos, snip_end_pos = node.parent.snippet.mark:pos_begin_end()
-      local pos = win_get_cursor(0)
-      pos[1] = pos[1] - 1 -- LuaSnip is 0-based not 1-based like nvim for rows
-      return pos[1] >= snip_begin_pos[1] and pos[1] <= snip_end_pos[1]
-    end
-
-    local function seek_luasnip_cursor_node()
-      local pos = win_get_cursor(0)
-      pos[1] = pos[1] - 1
-      local node = luasnip.session.current_nodes[get_current_buf()]
-      if not node then
-        return false
-      end
-
-      local snippet = node.parent.snippet
-      local exit_node = snippet.insert_nodes[0]
-
-      if exit_node then
-        local exit_pos_end = exit_node.mark:pos_end()
-        if (pos[1] > exit_pos_end[1]) or (pos[1] == exit_pos_end[1] and pos[2] > exit_pos_end[2]) then
-          snippet:remove_from_jumplist()
-          luasnip.session.current_nodes[get_current_buf()] = nil
-
-          return false
-        end
-      end
-
-      node = snippet.inner_first:jump_into(1, true)
-      while node ~= nil and node.next ~= nil and node ~= snippet do
-        local n_next = node.next
-        local next_pos = n_next and n_next.mark:pos_begin()
-        local candidate = n_next ~= snippet and next_pos and (pos[1] < next_pos[1])
-          or (pos[1] == next_pos[1] and pos[2] < next_pos[2])
-
-        if n_next == nil or n_next == snippet.next then
-          snippet:remove_from_jumplist()
-          luasnip.session.current_nodes[get_current_buf()] = nil
-
-          return false
-        end
-
-        if candidate then
-          luasnip.session.current_nodes[get_current_buf()] = node
-          return true
-        end
-
-        local ok
-        ok, node = pcall(node.jump_from, node, 1, true)
-        if not ok then
-          snippet:remove_from_jumplist()
-          luasnip.session.current_nodes[get_current_buf()] = nil
-
-          return false
-        end
-      end
-
-      if exit_node then
-        luasnip.session.current_nodes[get_current_buf()] = snippet
-        return true
-      end
-
-      snippet:remove_from_jumplist()
-      luasnip.session.current_nodes[get_current_buf()] = nil
-      return false
-    end
-
-    if backwards then
-      return inside_snippet() and luasnip.jumpable(-1)
-    else
-      return inside_snippet() and seek_luasnip_cursor_node() and luasnip.jumpable()
-    end
+  local function has_words_before()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
   end
 
   local window_options = {
     border = "single",
     scrolloff = 1,
+    col_offset = -3,
   }
 
-  local setup_options
-  setup_options = {
-    completion = {
-      keyword_length = 1,
-    },
-    experimental = {
-      ghost_text = true,
-      native_menu = false,
-    },
+  cmp.setup {
     formatting = {
       fields = { "kind", "abbr", "menu" },
-      max_width = 0,
-      kind_icons = {
-        Class = " ",
-        Color = " ",
-        Constant = "ﲀ ",
-        Constructor = " ",
-        Enum = "練",
-        EnumMember = " ",
-        Event = " ",
-        Field = " ",
-        File = "",
-        Folder = " ",
-        Function = " ",
-        Interface = "ﰮ ",
-        Keyword = " ",
-        Method = " ",
-        Module = " ",
-        Operator = " ",
-        Property = " ",
-        Reference = " ",
-        Snippet = " ",
-        Struct = " ",
-        Text = " ",
-        TypeParameter = " ",
-        Unit = "塞",
-        Value = " ",
-        Variable = " ",
-      },
-      source_names = {
-        nvim_lsp = "(LSP)",
-        emoji = "(Emoji)",
-        path = "(Path)",
-        calc = "(Calc)",
-        cmp_tabnine = "(Tabnine)",
-        vsnip = "(Snippet)",
-        luasnip = "(Snippet)",
-        buffer = "(Buffer)",
-        tmux = "(TMUX)",
-      },
-      duplicates = {
-        buffer = 1,
-        path = 1,
-        nvim_lsp = 0,
-        luasnip = 1,
-      },
-      duplicates_default = 0,
       format = function(entry, vim_item)
-        local max_width = setup_options.formatting.max_width
-        if max_width ~= 0 and #vim_item.abbr > max_width then
-          vim_item.abbr = ("%s…"):format(string.sub(vim_item.abbr, 1, max_width - 1))
-        end
-        vim_item.kind = setup_options.formatting.kind_icons[vim_item.kind]
-        vim_item.menu = setup_options.formatting.source_names[entry.source.name]
-        vim_item.dup = setup_options.formatting.duplicates[entry.source.name]
-          or setup_options.formatting.duplicates_default
-        return vim_item
-      end,
-    },
-    snippet = {
-      expand = function(args)
-        luasnip.lsp_expand(args.body)
+        local kind = lspkind.cmp_format { mode = "symbol_text", maxwidth = 50 }(entry, vim_item)
+        local strings = vim.split(kind.kind, "%s", { trimempty = true })
+        kind.kind = strings[1] or ""
+        kind.menu = "(" .. (strings[2] or "") .. ")"
+        return kind
       end,
     },
     window = {
@@ -198,48 +56,46 @@ function M.config()
       ["<C-d>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<Tab>"] = cmp.mapping(function(fallback)
-        if luasnip.expandable() then
-          luasnip.expand()
-        elseif jumpable() then
-          luasnip.jump(1)
-        elseif cmp.visible() then
+        if cmp.visible() then
           cmp.select_next_item()
+        elseif luasnip.expand_or_locally_jumpable() then
+          luasnip.expand_or_jump()
+        elseif has_words_before() then
+          cmp.complete()
         else
           fallback()
         end
       end, { "i", "s" }),
       ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if jumpable(true) then
-          luasnip.jump(-1)
-        elseif cmp.visible() then
+        if cmp.visible() then
           cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
         else
           fallback()
         end
       end, { "i", "s" }),
-
-      ["<C-l>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.abort(),
-      ["<CR>"] = cmp.mapping(function(fallback)
-        if cmp.visible() and cmp.confirm(setup_options.confirm_opts) then
-          if jumpable() then
-            luasnip.jump(1)
-          end
-          return
-        end
-
-        if jumpable() then
-          if not luasnip.jump(1) then
+      ["<C-l>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          else
             fallback()
           end
+        else
+          cmp.complete()
+        end
+      end),
+      ["<C-h>"] = cmp.mapping(function(fallback)
+        if cmp.visible() and luasnip.jumpable(-1) then
+          luasnip.jump(-1)
         else
           fallback()
         end
       end),
+      ["<C-e>"] = cmp.mapping.abort(),
     },
   }
-
-  cmp.setup(setup_options)
 end
 
 return M

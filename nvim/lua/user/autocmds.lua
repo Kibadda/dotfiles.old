3c8640805064379a1187ef09adc46231bfec2e16
Michael Strobel <mstrobel97@gmail.com>
2023-02-06 15:35:47 +0100

    🥇 remove lastplace and use autocmd

--- nvim/lua/user/autocmds.lua
+++ nvim/lua/user/autocmds.lua
@@ -77,3 +77,14 @@ autocmd("BufEnter", {
     }
   end,
 })
+
+autocmd("BufReadPost", {
+  group = augroup("GotoLastPlace", { clear = true }),
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})

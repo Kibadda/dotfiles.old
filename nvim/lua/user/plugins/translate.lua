0797eab20e9db20a3688914db99b64d73068eab8
Michael Strobel <mstrobel97@gmail.com>
2023-02-13 14:01:41 +0100

    💪 fix translate error

--- nvim/lua/user/plugins/translate.lua
+++ nvim/lua/user/plugins/translate.lua
@@ -18,16 +18,18 @@ function M.init()
       },
     },
   }
-
-  require("user.utils.globals").set("", {
-    deepl_api_auth_key = vim.env.DEEPL_API_KEY,
-  })
 end
 
-M.opts = {
-  default = {
-    command = "deepl_free",
-  },
-}
+function M.config()
+  require("user.utils.globals").set("", {
+    deepl_api_auth_key = vim.env.DEEPL_API_AUTH_KEY,
+  })
+
+  require("translate").setup {
+    default = {
+      command = "deepl_free",
+    },
+  }
+end
 
 return M

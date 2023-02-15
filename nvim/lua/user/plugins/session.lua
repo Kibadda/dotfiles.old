935e6c8bf055f5edad7e0d92a94a4c0813fdb0e7
Michael Strobel <mstrobel97@gmail.com>
2023-02-15 08:13:22 +0100

    🎑 move session keybinds to session.lua

--- nvim/lua/user/plugins/session.lua
+++ nvim/lua/user/plugins/session.lua
@@ -5,6 +5,30 @@ local M = {
   priority = 1000,
 }
 
+function M.init()
+  require("user.utils.register").keymaps {
+    n = {
+      ["<Leader>"] = {
+        S = {
+          name = "Session",
+          n = {
+            function()
+              require("session.core").new()
+            end,
+            "New",
+          },
+          l = {
+            function()
+              require("session.telescope").list()
+            end,
+            "New",
+          },
+        },
+      },
+    },
+  }
+end
+
 M.opts = {
   hooks = {
     pre = {
